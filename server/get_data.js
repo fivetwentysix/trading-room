878c57b7c5ae1eaf5ac8e27615fe267c33c2ab5f
Patrick Ma <fivetwentysix@gmail.com>
2020-02-05 03:29:23 +0000

    get data script

---
+++ server/get_data.js
@@ -0,0 +1,45 @@
+// Get intraday data for the SPY.
+
+const moment = require('moment');
+const https = require('https');
+const querystring = require('querystring');
+const process = require('process');
+const fs = require('fs');
+
+const symbol = 'SPY';
+const apikey = process.env.AMERITRADE_API_KEY;
+
+console.log(`[${moment()}] Trading Room Fetching Intraday ${symbol} Data`);
+
+const query = querystring.stringify({
+  apikey,
+  periodType: 'day',
+  period: 10,
+  frequencyType: 'minute',
+  frequency: 1
+});
+
+const options = {
+  hostname: 'api.tdameritrade.com',
+  port: 443,
+  path: `/v1/marketdata/${symbol}/pricehistory?${query}`,
+  method: 'GET'
+};
+
+const req = https.request(options, (res) => {
+  let data = new String();
+
+  res.on('data', (d) => {
+    data = data + String(d);
+  });
+
+  res.on('end', () => {
+    fs.writeFileSync(`${__dirname}/data.json`, data);
+  });
+});
+
+req.on('error', (e) => {
+  console.error(e);
+});
+
+req.end();
