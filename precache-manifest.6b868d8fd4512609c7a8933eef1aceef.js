896391d04cd1a72e351e6fc3820d76ffb65eaf92
Patrick Ma <fivetwentysix@gmail.com>
2020-02-05 05:35:49 +0000

    Updates

---
+++ precache-manifest.6b868d8fd4512609c7a8933eef1aceef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ad6df2f0d76d275445c2d35a53454f",
+    "url": "/trading-room/index.html"
+  },
+  {
+    "revision": "ddaba669a08a2f843f7f",
+    "url": "/trading-room/static/css/main.91e4f968.chunk.css"
+  },
+  {
+    "revision": "a35fcab568aa77beb61e",
+    "url": "/trading-room/static/js/2.053f7dfb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/trading-room/static/js/2.053f7dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddaba669a08a2f843f7f",
+    "url": "/trading-room/static/js/main.e1731717.chunk.js"
+  },
+  {
+    "revision": "ef150c46cf90d31ff2c8",
+    "url": "/trading-room/static/js/runtime-main.bd736002.js"
+  }
+]);
